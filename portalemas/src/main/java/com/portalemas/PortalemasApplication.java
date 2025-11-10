220f2b069aeb480be5f27a15d4bfef0f3395b956
Franco <franco.cantarello@gmail.com>
2025-11-10 12:42:38 +0100

    Primo Commit

---
+++ portalemas/src/main/java/com/portalemas/PortalemasApplication.java
@@ -0,0 +1,13 @@
+package com.portalemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalemasApplication.class, args);
+	}
+
+}
---
+++ portalemas/src/test/java/com/portalemas/PortalemasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portalemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
